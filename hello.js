fd0690360a0b7fd85dc3267f1b8c1ef2d5bd6c7e
dipranjanJS <dipranjan16oct@gmail.com>
2020-08-06 20:46:57 +0530

    Efficient way to move zeroes at the end of an array

--- hello.js
+++ hello.js
@@ -1,15 +1,22 @@
-let myArr = [1,2,0,3,0,5];
-
+var myArr = [1,2,0,3,0,5];
 document.getElementById('para').textContent = myArr;
 
-function moveZerosToEnd(arr) {
-    for(i = 0 ; i < arr.length ; i++) {
-        if(arr[i] === 0) {
-            arr.splice(i,1);
-            arr.push(0);
-            console.log(arr);
+function moveZerosToEnd(nums) {
+    count = 0;
+    j = 0;
+    flag = 0;
+    while(j < nums.length) {
+        if(nums[j] === 0 && flag === 0) {
+            count = j;
+            flag += 1;
         }
+        if(nums[j] !== 0 && flag !== 0) {
+            let a = nums[count];
+            nums[count] = nums[j];
+            nums[j] = a;
+            count += 1
+        }
+        j += 1
     }
-
-    document.getElementById('para').textContent = arr;
+    document.getElementById('para').textContent = nums;
 }
